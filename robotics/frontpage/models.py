87368636ae963ec6edbc733013b5635057ee1b76
Edward Liang <edward.k.liang@gmail.com>
2014-01-09 00:37:06 -0800

    images working

--- robotics/frontpage/models.py
+++ robotics/frontpage/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 
-class Carousel(models.Model):
-    image = models.ImageField(upload_to = 'assets/images/')
-    def __unicode__(self):
-        return self.image
-
 class Picture(models.Model):
-    image = models.ImageField(upload_to = 'assets/images/')
-    def __unicode__(self):
-        return self.image
+    image = models.ImageField(upload_to = 'images/')
 
 class Caption(models.Model):
     text = models.CharField(max_length=200)
